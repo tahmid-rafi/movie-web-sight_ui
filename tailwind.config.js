25ee77fa1d1973e5fdc68eb659055c58d616acd4
tahmid-rafi <trafi366@gmail.com>
2025-03-23 22:47:15 +0600

    solve error

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // আপনার ফাইল পাথ অনুযায়ী পরিবর্তন করুন
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
